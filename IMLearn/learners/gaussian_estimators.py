61efbecf487cbfdebfec01a6435d8dd3677c4a92
Liel Amar <liel@lielamar.com>
2023-03-19 14:18:37 +0200

    .

--- IMLearn/learners/gaussian_estimators.py
+++ IMLearn/learners/gaussian_estimators.py
@@ -7,7 +7,8 @@ class UnivariateGaussian:
     """
     Class for univariate Gaussian Distribution Estimator
     """
-    def __init__(self, biased_var: bool = False) -> UnivariateGaussian:
+
+    def __init__(self, biased_var: bool = False):
         """
         Estimator for univariate Gaussian mean and variance parameters
 
@@ -30,6 +31,7 @@ class UnivariateGaussian:
             Estimated variance initialized as None. To be set in `UnivariateGaussian.fit`
             function.
         """
+
         self.biased_ = biased_var
         self.fitted_, self.mu_, self.var_ = False, None, None
 
@@ -51,7 +53,16 @@ class UnivariateGaussian:
         Sets `self.mu_`, `self.var_` attributes according to calculated estimation (where
         estimator is either biased or unbiased). Then sets `self.fitted_` attribute to `True`
         """
-        raise NotImplementedError()
+
+        self.mu_ = X.mean()
+
+        # If self.biased_, we want to use a biased variance estimator.
+        # In class (Course book, page 13) we saw that a biased variance estimator is: (1 / m) * sum((Xi - u)^2)
+        #                                      and an unbiased variance estimator is: (1 / (m - 1)) * sum((Xi - u)^2)
+
+        # We can use 'ddof' (which affects the denominator) to achieve biased/unbiased variance.
+        # * The denominator used by numpy.ndarray.var is: "m - ddof"
+        self.var_ = X.var(ddof=0) if self.biased_ else X.var(ddof=1)
 
         self.fitted_ = True
         return self
@@ -74,9 +85,21 @@ class UnivariateGaussian:
         ------
         ValueError: In case function was called prior fitting the model
         """
+
         if not self.fitted_:
             raise ValueError("Estimator must first be fitted before calling `pdf` function")
-        raise NotImplementedError()
+
+        # The PDF of a normal distribution is (Course book, page 11):
+        # (1 / sqrt(2 * pi * sigma^2)) * exp((-1 / 2 * sigma^2) * (X - mu)^2)
+
+        # * Notice that X is a random vector. Thus, when using np.power(X - self.mu_, 2), we square each
+        #   random variable (each entry) individually. As a result, we end up with an m-long vector
+
+        coefficient = 1 / np.sqrt(2 * np.pi * self.var_)
+        exp_coefficient = -1 / (2 * self.var_)
+        exp_error_squared = np.power(X - self.mu_, 2)
+
+        return coefficient * np.exp(exp_coefficient * exp_error_squared)
 
     @staticmethod
     def log_likelihood(mu: float, sigma: float, X: np.ndarray) -> float:
@@ -97,13 +120,23 @@ class UnivariateGaussian:
         log_likelihood: float
             log-likelihood calculated
         """
-        raise NotImplementedError()
+
+        # To calculate log-likelihood, we can calculate the likelihood and then extract its logarithm.
+        # The normal distribution likelihood function is (Course book, page 15):
+        # (1 / (2 * pi * sigma^2)^(m/2)) * exp((-1/2 * sigma^2) * sum(xi - mu)^2)
+
+        coefficient = 1 / np.power(2 * np.pi * np.power(sigma, 2), len(X) / 2)
+        exp_coefficient = -1 / (2 * np.power(sigma, 2))
+        exp_error_sum = np.sum(np.power(X - mu, 2))
+
+        return np.log(coefficient * np.exp(exp_coefficient * exp_error_sum))
 
 
 class MultivariateGaussian:
     """
     Class for multivariate Gaussian Distribution Estimator
     """
+
     def __init__(self):
         """
         Initialize an instance of multivariate Gaussian estimator
@@ -122,8 +155,8 @@ class MultivariateGaussian:
             Estimated covariance initialized as None. To be set in `MultivariateGaussian.fit`
             function.
         """
-        self.mu_, self.cov_ = None, None
-        self.fitted_ = False
+
+        self.fitted_, self.mu_, self.cov_ = False, None, None
 
     def fit(self, X: np.ndarray) -> MultivariateGaussian:
         """
@@ -143,7 +176,17 @@ class MultivariateGaussian:
         Sets `self.mu_`, `self.cov_` attributes according to calculated estimation.
         Then sets `self.fitted_` attribute to `True`
         """
-        raise NotImplementedError()
+
+        # The mean in a Multivariate Gaussian distribution, is a vector (of length D) of the means.
+        # We can achieve this by calculating the mean over the 0 axis.
+        self.mu_ = X.mean(axis=0)
+
+        # The covariance matrix is a DxD matrix defined as follows:
+        # cov_[i, j] = (1 / (m - 1)) * sum((X[k][i] - u[i]) * (x[k][j] - u[j])
+        # Notice that we can also write cov_ as: (1 / (m - 1)) * (X - u)^T * (X - u)
+        # Using np.cov, we can get this result easily. It computes the average (mean) of X in each dimension,
+        # then calculates (X - u) and computes its dot product, dividing by (m - 1)
+        self.cov_ = np.cov(X, rowvar=False, bias=False)
 
         self.fitted_ = True
         return self
@@ -166,9 +209,19 @@ class MultivariateGaussian:
         ------
         ValueError: In case function was called prior fitting the model
         """
+
         if not self.fitted_:
             raise ValueError("Estimator must first be fitted before calling `pdf` function")
-        raise NotImplementedError()
+
+        # The PDF of a normal, multivariate distribution is (Course book, page 23):
+        # (1 / sqrt((2 * pi)^d * |cov|)) * exp((-1 / 2) * (X - mu)^t * (cov)^-1 * (X - mu))
+        # * Notice that X is a random vector, each entry is a vector of size D.
+
+        d = len(self.mu_)
+        coefficient = 1 / np.sqrt(np.power(2 * np.pi, d)) * det(self.cov_)
+        exp_coefficient = (-1 / 2) * ((X - self.mu_).T @ inv(self.cov_) @ (X - self.mu_))
+
+        return coefficient * np.exp(exp_coefficient)
 
     @staticmethod
     def log_likelihood(mu: np.ndarray, cov: np.ndarray, X: np.ndarray) -> float:
@@ -189,4 +242,17 @@ class MultivariateGaussian:
         log_likelihood: float
             log-likelihood calculated over all input data and under given parameters of Gaussian
         """
-        raise NotImplementedError()
+
+        # To calculate log-likelihood, we can use the formula from Q13 in this exercise (similar to wikipedia's)
+        # The normal, multivariate distribution log-likelihood function is:
+        # (-m / 2) * (ln(|cov|) + ln((2 * pi)^d)) + (-1 / 2) * sum((Xi - u)^T * cov^-1 * (Xi - u)))
+
+        m = len(X)
+        d = len(mu)
+
+        first_term = (-m / 2) * (np.log(det(cov)) + d * np.log(2 * np.pi))
+        # (X - mu) is a Matrix of size 1000x4. Therefore, it's multiplication with inv(cov) (4x4 matrix) is
+        # well-defined. We then want to multiply it by terms with (X - mu), resulting in the desired sum.
+        second_term = np.sum((X - mu) @ inv(cov) * (X - mu))
+
+        return first_term + second_term
--- exercises/fit_gaussian_estimators.py
+++ exercises/fit_gaussian_estimators.py
@@ -1,33 +1,70 @@
 from IMLearn.learners import UnivariateGaussian, MultivariateGaussian
 import numpy as np
-import plotly.graph_objects as go
+import pandas as pd
+import plotly.express as px
 import plotly.io as pio
+
 pio.templates.default = "simple_white"
 
 
 def test_univariate_gaussian():
+    mu, sigma = 10, 1
+    samples = np.random.normal(mu, sigma, 1000)
+
     # Question 1 - Draw samples and print fitted model
-    raise NotImplementedError()
+    uni_gaus = UnivariateGaussian()
+    uni_gaus.fit(samples)
+
+    print(f'({uni_gaus.mu_}, {uni_gaus.var_})')
 
     # Question 2 - Empirically showing sample mean is consistent
-    raise NotImplementedError()
+    estimations_error = [np.abs(uni_gaus.fit(samples[:sample_size]).mu_ - mu) for sample_size in range(10, 1001, 10)]
+
+    estimations_df = pd.DataFrame(np.array([np.arange(10, 1001, 10), np.array(estimations_error)]).transpose(),
+                                  columns=["smpl_size", "est_err"])
+
+    figure = px.line(estimations_df, x="smpl_size", y="est_err",
+                     labels={"smpl_size": "Sample Size", "est_err": "Estimation Error (Distance from real MU)"},
+                     title="Estimation Error as a function of Sample Size")
+    figure.show()
 
     # Question 3 - Plotting Empirical PDF of fitted model
-    raise NotImplementedError()
+    pdf_values = uni_gaus.pdf(samples)
+    pdf_df = pd.DataFrame(np.array([samples, pdf_values]).transpose(), columns=["smpl", "pdf"])
+
+    figure = px.scatter(pdf_df, x="smpl", y="pdf",
+                        labels={"smpl": "Sample Value", "pdf": "Calculated PDF Value"},
+                        title="PDF Values of a set of 1000 samples distributed Normal(10, 1)")
+    figure.show()
 
 
 def test_multivariate_gaussian():
+    mu = np.array([0, 0, 4, 0])
+    cov = np.array([[1, 0.2, 0, 0.5], [0.2, 2, 0, 0], [0, 0, 1, 0], [0.5, 0, 0, 1]])
+    samples = np.random.multivariate_normal(mu, cov, 1000)
+
     # Question 4 - Draw samples and print fitted model
-    raise NotImplementedError()
+    mult_gaus = MultivariateGaussian()
+    mult_gaus.fit(samples)
+
+    print(f'Estimated Expectation: {mult_gaus.mu_}')
+    print(f'Estimated Covariance Matrix: {mult_gaus.cov_}')
+
 
     # Question 5 - Likelihood evaluation
-    raise NotImplementedError()
+    f1 = np.linspace(-10, 10, 200)
+    f3 = np.linspace(-10, 10, 200)
+
+    for i in range(200):
+        mu = np.array([f1[i], 0, f3[i], 0])
+
+        print(mult_gaus.log_likelihood(mu, cov, samples))
 
     # Question 6 - Maximum likelihood
-    raise NotImplementedError()
+    # raise NotImplementedError()
 
 
 if __name__ == '__main__':
     np.random.seed(0)
-    test_univariate_gaussian()
+    # test_univariate_gaussian()
     test_multivariate_gaussian()
