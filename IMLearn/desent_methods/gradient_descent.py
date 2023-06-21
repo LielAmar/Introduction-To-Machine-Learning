Finished Ex5 2.1.1 - Fixed gradient decent

IMLearn/desent_methods/gradient_descent.py
@@ -124,38 +124,45 @@ class GradientDescent:
 
         """
 
-        best_weight = None
-        best_val = None
-        last_weight = None
-        mean_weight = None
+        # Initiating first weights vector
+        best_weight, best_val = f.weights, f.compute_output(X=X, y=y)
+        last_weight = f.weights
+        mean_weight = f.weights
         iterations = 0
 
-        weight = f.weights
+        weight_t = f.weights
 
         for t in range(self.max_iter_):
-            val = f.compute_output(X=X, y=y)
+            # Calculate the gradient and eta to be used to update current weight
             grad = f.compute_jacobian(X=X, y=y)
-
             eta = self.learning_rate_.lr_step(t=t)
-            delta = np.linalg.norm((np.power(weight, t) - np.power(weight, t-1)) ,ord=2)
 
-            self.callback_(solver=self, weights=weight, val=val, grad=grad, t=t, eta=eta, delta=delta)
+            # Update current weights vector
+            f.weights = weight_t - eta * grad
 
-            last_weight = weight
-            mean_weight = weight if mean_weight is None else (mean_weight + weight)
-            best_weight = weight if best_weight is None else (weight if best_val < val else best_weight)
+            # Calculate the performance and delta of the new weights vector
+            val = f.compute_output(X=X, y=y)
+            delta = np.linalg.norm(f.weights - weight_t, ord=2)
+
+            # Call the callback function
+            self.callback_(solver=self, weights=f.weights, val=val, grad=grad, t=t, eta=eta, delta=delta)
+
+            # Update the last weights vector, mean weights vector and best weights vector
+            last_weight = f.weights
+            mean_weight = f.weights if mean_weight is None else (mean_weight + f.weights)
+            best_weight = f.weights if best_weight is None else (f.weights if best_val < val else best_weight)
             best_val = val if best_val is None else (val if best_val < val else best_val)
 
             iterations = iterations + 1
+
+            # Update the weight_t to be the weights vector calculated at this iteration
+            weight_t = f.weights
 
             # If the delta (change between weight vectors) is less than tol, break the loop
             if delta < self.tol_:
                 break
 
-            weight = weight - eta * grad
-
-
-        # Return the correct weight according to out type
+        # Return the correct weights vector according to out type
         if self.out_type_ == "last":
             return last_weight
         elif self.out_type_ == "best":

exercises/gradient_descent_investigation.py
@@ -73,12 +73,49 @@ def get_gd_state_recorder_callback() -> Tuple[Callable[[], None], List[np.ndarra
     weights: List[np.ndarray]
         Recorded parameters
     """
-    raise NotImplementedError()
+
+    recorded_values = []
+    recorded_weights = []
+
+    def gd_callback(solver, weights, val, grad, t, eta, delta):
+        recorded_values.append(val)
+        recorded_weights.append(weights)
+
+    return gd_callback, recorded_values, recorded_weights
 
 
 def compare_fixed_learning_rates(init: np.ndarray = np.array([np.sqrt(2), np.e / 3]),
                                  etas: Tuple[float] = (1, .1, .01, .001)):
-    raise NotImplementedError()
+
+    empty = np.empty(shape=(0,))
+
+    for eta in etas:
+        for module, name in [(L1, "L1"), (L2, "L2")]:
+            gd_callback, values, weights = get_gd_state_recorder_callback()
+
+            gd = GradientDescent(learning_rate=FixedLR(eta), callback=gd_callback)
+            gd.fit(module(weights=init), X=empty, y=empty)
+
+            # Question 1: Plot the descent path
+            fig = plot_descent_path(module=module,
+                                    descent_path=np.array(weights),
+                                    title=f"of an {name} module with fixed learning rate η={eta}")
+            fig.write_image(f"./ex5_graphs/{name}_{eta}_descent.png")
+
+            # Question 3: Plot the convergence rate
+            fig = go.Figure([go.Scatter(y=values, mode="markers")],
+                            layout=go.Layout(title=f"Convergence Rate of an {name} module with " 
+                                                   f"fixed learning rate η={eta}"))
+
+            fig.update_layout(width=650, height=500) \
+                .update_xaxes(title_text="Iteration") \
+                .update_yaxes(title_text=f"Convergence (w {name} norm)")
+
+            fig.write_image(f"./ex5_graphs/{name}_{eta}_convergence.png")
+
+            # Question 4: Lowest loss achieved
+            print(f"The lowest loss achieved by an {name} module with a fixed learning rate η={eta} "
+                  f"is {np.round(np.min(values), 3)}")
 
 
 def compare_exponential_decay_rates(init: np.ndarray = np.array([np.sqrt(2), np.e / 3]),
@@ -141,5 +178,5 @@ def fit_logistic_regression():
 if __name__ == '__main__':
     np.random.seed(0)
     compare_fixed_learning_rates()
-    compare_exponential_decay_rates()
-    fit_logistic_regression()
+    # compare_exponential_decay_rates()
+    # fit_logistic_regression()